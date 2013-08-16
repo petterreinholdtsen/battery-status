bc4153ce4415f03f1301ce2e39b02c12fb60e71b
Petter Reinholdtsen <pere@hungry.com>
2013-08-16 12:52:47 +0000

    Increase collecting frequency from every 15th to every 10th minute.

--- debian/hjemmenett-battery-status.cron.d
+++ debian/hjemmenett-battery-status.cron.d
@@ -1,2 +1,2 @@
 # Collect battery status ever 15 minutes
-*/15 * * * *     root    if [ -x /usr/sbin/collect-battery-status ]; then /usr/sbin/collect-battery-status ; fi
+*/10 * * * *     root    if [ -x /usr/sbin/collect-battery-status ]; then /usr/sbin/collect-battery-status ; fi
