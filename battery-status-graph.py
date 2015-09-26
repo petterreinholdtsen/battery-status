e9c85e87837b732a6f06f549c940e678582ae54a
Antoine Beaupré <anarcat@koumbit.org>
2015-09-26 13:36:37 -0400

    style: word-wrap

--- battery-status-graph.py
+++ battery-status-graph.py
@@ -25,8 +25,8 @@ def parse_csv_np():
     return data
 
 def to_percent(y, position):
-    # Ignore the passed in position. This has the effect of scaling the default
-    # tick locations.
+    # Ignore the passed in position. This has the effect of scaling
+    # the default tick locations.
     s = str(100 * y)
 
     # The percent symbol needs escaping in latex
@@ -37,7 +37,8 @@ def to_percent(y, position):
 
 def plot():
     data = parse_csv_np()
-    # create vectorized converter (can take list-like objects as arguments)
+    # create vectorized converter (can take list-like objects as
+    # arguments)
     dateconv = np.vectorize(datetime.datetime.fromtimestamp)
     dates = dateconv(data['timestamp'])
 
@@ -46,9 +47,16 @@ def plot():
     # XXX: can't seem to plot all at once...
     #plt.plot(dates, data['energy_now'], '-b', data['energy_full'], '-r')
     # ... but once at a time seems to do the result i am looking for
-    plt.plot(dates, data['energy_full_design'] / data['energy_full_design'] , '-k', label='design')
-    plt.plot(dates, data['energy_now']  / data['energy_full_design'], '-', color='grey', label='current')
-    plt.plot(dates, data['energy_full'] / data['energy_full_design'], '-r', label='effective')
+    plt.plot(dates, data['energy_full_design'] / data['energy_full_design'],
+             '-',
+             label='design')
+    plt.plot(dates, data['energy_now'] / data['energy_full_design'],
+             '-',
+             color='grey',
+             label='current')
+    plt.plot(dates, data['energy_full'] / data['energy_full_design'],
+             '-r',
+             label='effective')
     plt.legend(loc='lower left')
 
     # Tell matplotlib to interpret the x-axis values as dates
@@ -60,8 +68,9 @@ def plot():
     ax.set_ylabel('percent')
     ax.set_title('Battery capacity statistics')
 
-    # Create the formatter using the function to_percent. This multiplies all the
-    # dfault labels by 100, making them all percentages
+    # Create the formatter using the function to_percent. This
+    # multiplies all the dfault labels by 100, making them all
+    # percentages
     formatter = FuncFormatter(to_percent)
 
     # Set the formatter
